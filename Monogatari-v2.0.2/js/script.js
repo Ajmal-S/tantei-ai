bc76cba51823ee37a5699eeb5c5f195a95b3ac26
Ajmal-S <ajmal.sohail2294@gmail.com>
2023-08-26 17:44:47 -0400

    changes

--- Monogatari-v2.0.2/js/script.js
+++ Monogatari-v2.0.2/js/script.js
@@ -1,52 +1,52 @@
 /* global monogatari */
 let COHERE_API_KEY = "6eMUUH6enfvEKAdXQLlKR9lgRAJ5gwRnKy8o6i09";
 // Define the messages used in the game.
-async function asynchttpPost(context) {
-  try {
-    const payload = {
-      max_tokens: 70,
-      truncate: "END",
-      return_likelihoods: "NONE",
-      prompt: context,
-      stream: true,
-    };
-    const response = await fetch("https://api.cohere.ai/v1/generate", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + COHERE_API_KEY,
-      },
-      body: JSON.stringify(payload),
-    });
+// async function asynchttpPost(context) {
+//   try {
+//     const payload = {
+//       max_tokens: 70,
+//       truncate: "END",
+//       return_likelihoods: "NONE",
+//       prompt: context,
+//       stream: true,
+//     };
+//     const response = await fetch("https://api.cohere.ai/v1/generate", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: "Bearer " + COHERE_API_KEY,
+//       },
+//       body: JSON.stringify(payload),
+//     });
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    monogatari.storage().player.generated_response = "";
-    const reader = response.body.getReader();
-    while (true) {
-      const { value, done } = await reader.read();
-      let str = "";
-      if (done) break;
-      for (var i = 0; i < value.byteLength; i++) {
-        str += String.fromCharCode(value[i]);
-      }
-      result = JSON.parse(str);
-      monogatari.storage().player.generated_response += result.text;
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! status: ${response.status}`);
+//     }
+//     monogatari.storage().player.generated_response = "";
+//     const reader = response.body.getReader();
+//     while (true) {
+//       const { value, done } = await reader.read();
+//       let str = "";
+//       if (done) break;
+//       for (var i = 0; i < value.byteLength; i++) {
+//         str += String.fromCharCode(value[i]);
+//       }
+//       result = JSON.parse(str);
+//       monogatari.storage().player.generated_response += result.text;
 
-      console.log(result.text);
-      // alert('Received', value);
-      if (result.is_finished === "COMPLETE") break;
-    }
-    //   const data = await response.json();
-    //   return data;
-  } catch (error) {
-    console.error(`Fetch failed: ${error}`);
-    throw error;
-  }
-}
+//       console.log(result.text);
+//       // alert('Received', value);
+//       if (result.is_finished === "COMPLETE") break;
+//     }
+//     //   const data = await response.json();
+//     //   return data;
+//   } catch (error) {
+//     console.error(`Fetch failed: ${error}`);
+//     throw error;
+//   }
+// }
 
-function httpPostSync(context) {
+function coherePostRequest(context) {
   try {
     const xhr = new XMLHttpRequest();
     xhr.open("POST", " https://api.cohere.ai/v1/generate", false); // Setting the third parameter to false makes the request synchronous
@@ -155,14 +155,48 @@ monogatari.assets("videos", {});
 monogatari.assets("images", {});
 
 // Define the backgrounds for each scene.
-monogatari.assets("scenes", {});
+monogatari.assets("scenes", { inn: "main.jpg" });
 
 // Define the Characters
 monogatari.characters({
-  y: {
-    name: "Yui",
-    color: "#5bcaff",
+  anya: {
+    name: "Anya",
+    directory: "anya",
+    sprites: { main: "main.png" },
   },
+  hakim: {
+    name: "Hakim",
+    directory: "hakim",
+    sprites: { main: "main.png" },
+  },
+  james: {
+    name: "James",
+    directory: "james",
+    sprites: { main: "main.png" },
+  },
+  sofia: {
+    name: "Sofia",
+    directory: "sofia",
+    sprites: { main: "main.png" },
+  },
+  william: {
+    name: "William",
+    directory: "william",
+    sprites: { main: "main.png" },
+  },
+  anthonyAKAliam: {
+    name: "Anthony",
+    directory: "anthonyAKAliam",
+    sprites: { main: "main.png" },
+  },
+  narrator: {
+    name: "Narrator",
+    directory: "narrator",
+    sprites: { main: "main.png" },
+  },
+  detective:{
+	name: "Detective"
+  }
 });
 
 monogatari.storage({
@@ -170,13 +204,29 @@ monogatari.storage({
     name: "",
     intelligence: 0,
   },
+  character_chat_log: {
+	anya: [],
+	hakim: [],
+	james: [],
+	sofia: [],
+	william: [],
+	anthonyAKAliam: []
+  },
+  current_investigated_character: "narrator"
 });
 
-function sendAlert() {
+function makeCohereRequest() {
   // alert('This is pretty useful!');
   // alert(monogatari.storage().player.intelligence)
-  cohere_response = httpPostSync(monogatari.storage().player.player_response);
+  player_response = monogatari.storage().player.player_response
+  cohere_response = coherePostRequest(
+    player_response
+  );
+  current_investigated_character = monogatari.storage().current_investigated_character
+  monogatari.storage().character_chat_log[current_investigated_character].push("Detective: " + player_response)
+  monogatari.storage().character_chat_log[current_investigated_character].push(full_investigated_name + ": " + cohere_response)
   monogatari.storage().player.generated_response = cohere_response;
+  console.log(monogatari.storage().character_chat_log)
   // alert(cohere_response);
   return true;
 }
@@ -184,87 +234,128 @@ function sendAlert() {
 async function asyncSendAlert() {
   await asynchttpPost("This is an example of a vampire attack");
 }
-function bumpIntelligence() {
-  monogatari.storage().player.intelligence += 5;
+function onInvestigateChosen(character) {
+  monogatari.storage().current_investigated_character = character;
+}
+
+function checkIfFirstMessage(){
+	current_investigated_character = monogatari.storage().current_investigated_character
+	if (monogatari.storage().character_chat_log[current_investigated_character].length == 0){
+		first_message = "What would you like to know?"
+		full_investigated_name = monogatari.characters()[current_investigated_character].name
+		// monogatari.storage().character_chat_log[current_investigated_character].push(full_investigated_name + ": " + first_message)
+		monogatari.storage().player.generated_response = first_message
+	}
 }
 
 monogatari.script({
   // The game starts here.
   Start: [
-    "show scene #f7f6f6 with fadeIn",
-    // 'show notification Welcome',
-    // {
-    //   Input: {
-    //     Text: "What is your name?",
-    //     Validation: function (input) {
-    //       return input.trim().length > 0;
-    //     },
-    //     Save: function (input) {
-    //       this.storage({
-    //         player: {
-    //           name: input,
-    //           intelligence: 0,
-    //         },
-    //       });
-    //       return true;
-    //     },
-    //     Revert: function () {
-    //       this.storage({
-    //         player: {
-    //           name: "",
-    //           intelligence: 0,
-    //         },
-    //       });
-    //     },
-    //     Warning: "You must enter a name!",
-    //   },
-    // },
-    "y Hi {{player.name}} Welcome to Monogatari!",
-    "jump Yes",
-    // {
-    //   Choice: {
-    //     Dialog: "y Have you already read some documentation? (hell yeah)",
-    //     Yes: {
-    //       Text: "Yes",
-    //       Do: "jump Yes",
-    //     },
-    //     No: {
-    //       Text: "No",
-    //       Do: "jump No",
-    //     },
-    //   },
-    // },
+    "show scene inn with fadeIn",
+    "show character narrator main",
+    "narrator Hi {{player.name}} Welcome to Monogatari!",
+	"hide character narrator",
+    "show character anya main",
+    "anya anya intro",
+	"hide character anya",
+    "show character hakim main",
+    "hakim hakim intro",
+	"hide character hakim",
+    "show character narrator main",
+    "narrator Give player intro here",
+    "jump investigateChoice",
   ],
-
-  Yes: [
-    "y Thats awesome!",
-    "y Then you are ready to go ahead and create an amazing Game!",
-    "y I can’t wait to see what story you’ll tell!",
+  investigateChoice: [
+	"hide character {{current_investigated_character}}",
+    {
+	  
+      Choice: {
+        Dialog: "narrator Who do you want to investigate?",
+        Anya: {
+          Text: "Anya",
+          onChosen: function () {
+            onInvestigateChosen("anya");
+          },
+          Do: "jump InvestigateLoop",
+        },
+        Hakim: {
+          Text: "Hakim",
+          onChosen: function () {
+            onInvestigateChosen("hakim");
+          },
+          Do: "jump InvestigateLoop",
+        },
+        James: {
+          Text: "James",
+          onChosen: function () {
+            onInvestigateChosen("james");
+          },
+          Do: "jump InvestigateLoop",
+        },
+        Sofia: {
+          Text: "Sofia",
+          onChosen: function () {
+            onInvestigateChosen("sofia");
+          },
+          Do: "jump InvestigateLoop",
+        },
+        William: {
+          Text: "William",
+          onChosen: function () {
+            onInvestigateChosen("william");
+          },
+          Do: "jump InvestigateLoop",
+        },
+        AnthonyAKAliam: {
+          Text: "Anthony",
+          onChosen: function () {
+            onInvestigateChosen("anthonyAKAliam");
+          },
+          Do: "jump InvestigateLoop",
+        },
+      },
+    },
+  ],
+  InvestigateLoop: [
+    "show character {{current_investigated_character}} main",
+	checkIfFirstMessage,
+	"{{current_investigated_character}} {{player.generated_response}}",
+    // "{{current_investigated_character}} Thats awesome!",
+    // "{{current_investigated_character}} Then you are ready to go ahead and create an amazing Game!",
+    // "{{current_investigated_character}} I can’t wait to see what story you’ll tell!",
+    {
+      Choice: {
+        Dialog: "narrator What do you want to do?",
+        changeInvestigatationTarget: {
+          Text: "Investigate someone else",
+          Do: "jump investigateChoice",
+        },
+        respondToCurrentTarget: {
+          Text: "Continue investigating {{current_investigated_character}}",
+          Do: "next",
+        },
+		makeFinalGuess: {
+			Text: "Guess the murderer",
+			Do: "jump MakeFinalGuess"
+		}
+      },
+    },
     {
       Input: {
         Text: "What is your response?",
-        // Validation: function (input) {
-        //   return input.trim().length > 0;
-        // },
         Save: function (input) {
-		  monogatari.storage().player.player_response = input
+          monogatari.storage().player.player_response = input;
           return true;
         },
       },
     },
-
-    sendAlert,
-    "y {{player.generated_response}}",
-    "jump Yes",
+	"detective {{player.player_response}}",
+    makeCohereRequest,
+    "jump InvestigateLoop",
   ],
-
-  No: [
-    "y You can do it now.",
-
-    "show message Help",
-
-    "y Go ahead and create an amazing Game!",
-    "y I can’t wait to see what story you’ll tell!",
-    "end",
+  MakeFinalGuess: [
+	"show character narrator main",
+	"narrator So you finally figured it out. Who is it?",
+	"end"
   ],
 });
